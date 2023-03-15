261621b1df4168d72311e2dcddcf5c8f21832b66
PUV00014\owner <lotus.life13@gmail.com>
2023-03-15 21:09:44 +0900

    いらんやつ削除

--- index.php
+++ index.php
@@ -81,42 +81,8 @@
 
             </div>
         </div>
-<<<<<<< HEAD
-        <div class="box2">
-            <img src="<?php echo get_template_directory_uri(); ?>/img_file/post_img_2.png" alt="village">
-            <p>2018/5/19</p>
-            <p>あのネオンはいつ交換するのか！？観覧 <br> 車の謎に迫る！</p>
-            <p>READ MORE</p>
-        </div>
-        <div class="box3">
-            <img src="<?php echo get_template_directory_uri(); ?>/img_file/post_img_3.png" alt="office">
-            <p>2018/5/18</p>
-            <p>ラソナの社内はこんなのよ</p>
-            <p>READ MORE</p>
-        </div>
-        <div class="box4">
-            <img src="<?php echo get_template_directory_uri(); ?>/img_file/post_img_4.png" alt="araha">
-            <p>2018/5/17</p>
-            <p>お隣のアラハはハワイ？</p>
-            <p>READ MORE</p>
-        </div>
-        <div class="box5">
-            <img src="<?php echo get_template_directory_uri(); ?>/img_file/post_img_5.png" alt="tent">
-            <p>2018/5/16</p>
-            <p>なぜテント？ラソナの人に聞いてみた</p>
-            <p>READ MORE</p>
-        </div>
-        <div class="box6">
-            <img src="<?php echo get_template_directory_uri(); ?>/img_file/post_img_6.png" alt="bayarea">
-            <p>2018/5/15</p>
-            <p>ベイエリアおしゃれすぎる問題</p>
-            <p>READ MORE</p>
-        </div>
-</div>
-=======
        
     </div>
->>>>>>> c4f6cbc9bdf33c6099335d6f35479829e3db67de
 </section>
 </div>
 </div>
